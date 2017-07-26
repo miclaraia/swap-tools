9dfc1af1e8a169baa8bf42f0f5f06a7af807ce71
Michael Laraia <micheal.laraia@gmail.com>
2017-07-26 16:38:52 +0100

    Added plotting skeleton

---
+++ swaptools/experiments/plot/plot.py
@@ -0,0 +1,20 @@
+
+
+
+class Plot:
+
+    def __init__(self, experiment):
+        self.experiment = experiment
+
+    ###############################################################
+
+    def extract(self):
+        pass
+
+    ###############################################################
+
+    def get_data(self):
+        pass
+
+    def run(self):
+        pass
