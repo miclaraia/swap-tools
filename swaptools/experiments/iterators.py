4ec97825fdc8a92050343204e32f5f25b7c0ea6c
Michael Laraia <micheal.laraia@gmail.com>
2017-08-15 16:59:50 -0500

    Added ValueIterator
    Class to manage the values unique to each trial. General structure
    to check if there is another item, increment this value, and count
    how many values are in the iterator.

---
+++ swaptools/experiments/iterators.py
@@ -0,0 +1,105 @@
+
+import math
+
+
+class ValueIterator:
+
+    ###############################################################
+
+    @staticmethod
+    def range(start, end, step):
+        return _Range(start, end, step)
+
+    @staticmethod
+    def list(values):
+        return _List(values)
+
+    ###############################################################
+
+
+class _Iterator:
+
+    def __init__(self, name=None):
+        self.name = name
+        self.current = None
+
+    def next(self):
+        pass
+
+    def step(self):
+        self.current = self.next()
+        return self.current
+
+    def more(self):
+        pass
+
+    def reset(self):
+        pass
+
+    def count(self):
+        pass
+
+    ###############################################################
+
+    def _name(self, name):
+        self.name = name
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        return self.step()
+
+
+class _Range(_Iterator):
+
+    def __init__(self, start, end, step, name=None):
+        super().__init__(name)
+        self.start = start
+        self.end = end
+        self._step = step
+
+        self.current = start
+
+    def next(self):
+        return self.current + self._step
+
+    def more(self):
+        return self.next() <= self.end
+
+    def reset(self):
+        self.current = self.start
+
+    def count(self):
+        return math.floor((self.end - self.start) / self._step) + 1
+
+
+class _List(_Iterator):
+
+    def __init__(self, values, name=None):
+        super().__init__(name)
+        self.values = values
+        self.i = 0
+
+    @property
+    def current(self):
+        return self.values[self.i]
+
+    def next(self):
+        if self.more():
+            return self.values[self.i + 1]
+
+    def step(self):
+        value = self.next()
+        self.i += 1
+
+        return value
+
+    def more(self):
+        return self.i < len(self.values)
+
+    def reset(self):
+        self.i = 0
+
+    def count(self):
+        return len(self.values)
