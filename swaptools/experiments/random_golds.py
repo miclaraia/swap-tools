be9d91ac04ffdeb6fa8285e0d3b78cbbb3dd9f2d
Michael Laraia <micheal.laraia@gmail.com>
2017-08-22 16:12:55 -0500

    Updated RandomGolds experiment
    Updated random golds experiment to new experiment framework, and
    updated tests.

--- swaptools/experiments/random_golds.py
+++ swaptools/experiments/random_golds.py
@@ -1,4 +1,5 @@
 
+from swaptools.experiments.iterators import ValueIterator
 from swaptools.experiments.experiment import Experiment
 from swaptools.experiments.experiment import Interace as _Interface
 
@@ -20,31 +21,20 @@ class RandomGolds(Experiment):
 
         self.trial_info.update({'golds': 0})
 
-    @staticmethod
-    def info_key_order():
-        return ['n', 'golds']
+    @classmethod
+    def new(cls, num_golds, series, *args, **kwargs):
+        e = super().new(*args, **kwargs)
 
-    def has_next(self, info):
-        n = info['n'] >= self.num_trials
-        golds = info['golds'] > self.num_golds[1]
+        series._name('series')
+        num_golds._name('golds')
 
-        return not (n or golds)
+        e.values = [series, num_golds]
 
-    def setup_first(self, info):
-        super().setup_first(info)
-        info['golds'] = self.num_golds[0]
-
-    def setup_increment(self, info):
-        super().setup_increment(info)
-        if info['n'] >= self.num_trials:
-            info['n'] = 0
-            info['golds'] += self.num_golds[2]
+        return e
 
     def setup_next(self):
-        super().setup_next()
         info = self.trial_info
 
-        logger.info('%s %s', str(info), str(self.num_trials))
         self.gg.reset()
         self.gg.random(info['golds'])
 
@@ -156,11 +146,12 @@ class Interface(_Interface):
             'description': description,
         }
         if args.num_golds:
-            golds = [int(i) for i in args.num_golds[0:3]]
-            kwargs['num_golds'] = tuple(golds)
+            a = [int(i) for i in args.num_golds[0:3]]
+            kwargs['num_golds'] = ValueIterator.range(*a)
 
         if args.num_trials:
-            kwargs['num_trials'] = int(args.num_trials[0])
+            series = int(args.num_trials[0])
+            kwargs['series'] = ValueIterator.range(1, series, 1)
 
         e = RandomGolds.new(**kwargs)
         e.run()
